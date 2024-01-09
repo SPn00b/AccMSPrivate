8f0329c4783b422a0d5bd4500120efe77ad92271
sanis <sanis@Sanish_PC>
2024-01-09 11:17:42 +0530

    TDD Spring Boot
    Basic start of TDD based Spring Boot project from
    https://www.youtube.com/watch?v=-H5sud1-K5A.

---
+++ TDD_Spring_Boot/src/main/java/com/example/demo/post/PostController.java
@@ -0,0 +1,16 @@
+package com.example.demo.post;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+	@GetMapping("")
+	List<Post> findAll() {
+		return null;
+	}
+}
---
+++ TDD_Spring_Boot/src/main/java/com/example/demo/post/PostRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.post;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface PostRepository extends ListCrudRepository<Post, Integer> {
+
+}
